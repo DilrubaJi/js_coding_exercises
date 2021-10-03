d7530e33a12c2e6ee9ea464f37a4389ede32baa9
DilrubaJi <91086050+DilrubaJi@users.noreply.github.com>
2021-10-03 18:13:48 +0100

    exercise004 5passed 5togo

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,22 +1,46 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let arrRes = [];
+  for (let i = 0; i < nums.length; i++){
+    if(nums[i] < 1){
+      arrRes.push(nums[i]);
+    }
+  }
+  return arrRes;
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  let arrRes = [];
+  for (let i = 0; i < names.length; i++){
+    if(names[i].substring(0,1) === char){
+      arrRes.push(names[i]);
+    }
+  }
+  return arrRes;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  let arrRes = [];
+  for (let i = 0; i < words.length; i++){
+    if(words[i].substring(0,3) === 'to '){
+      arrRes.push(words[i]);
+    }
+  }
+  return arrRes;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let arrRes = [];
+  for (let i = 0; i < nums.length; i++){
+    if(Number.isInteger(nums[i])){
+      arrRes.push(nums[i]);
+    }
+  }
+  return arrRes;
 }
 
 function getCities(users) {
