48159e38774e8b3b225072e04c1e99f2636a12e1
DilrubaJi <91086050+DilrubaJi@users.noreply.github.com>
2021-12-09 13:17:24 +0000

    failing (red) test

---
+++ test/exercise006.test.js
@@ -0,0 +1,16 @@
+const {
+    sumMultiples,
+    isValidDNA,
+    getComplementaryDNA,
+    isItPrime,
+    createMatrix,
+    areWeCovered
+} = require("../challenges/exercise006");
+
+describe("sumMultiples", () => {
+    test("it returns sum of any numbers that are a multiple of 3 or 5", () =>{
+        const result = sumMultiples([1, 3, 5]);
+        const expected = 8;
+        expected(result).toBe(expected);
+    });
+});
