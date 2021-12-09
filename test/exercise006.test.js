83f0e58a1cb995a37ff4dcd56fd65253920b1289
DilrubaJi <91086050+DilrubaJi@users.noreply.github.com>
2021-12-09 14:25:41 +0000

    expands tests for exercise006

--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -8,9 +8,46 @@ const {
 } = require("../challenges/exercise006");
 
 describe("sumMultiples", () => {
+    test("it throws an error if not passed an array", () => {
+        expect(() => {
+            sumMultiples();
+        }).toThrow("arr is required");
+
+        expect(() => {
+            sumMultiples("foo");
+        }).toThrow("an Array is required");
+
+        expect(() => {
+            sumMultiples(4);
+        }).toThrow("an Array is required");
+    });
+
     test("it returns sum of any numbers that are a multiple of 3 or 5", () =>{
         const result = sumMultiples([1, 3, 5]);
         const expected = 8;
         expect(result).toBe(expected);
     });
+
+    test("it works okay with decimal numbers", () => {
+        const result = sumMultiples([1, 3, 5.0, 2, 12 ,10]);
+        const expected = 30;
+        expect(result).toBe(expected);
+    });
+
+    test("return 0 if there are no multiples of 3 or 5", () => {
+        expect(sumMultiples([1, 2, 8, 13, 7])).toBe(0);
+    });
 });
+
+describe("areWeCovered", () => {
+    test("it returns false if there are no staff at all", () => {
+        expect(areWeCovered([], "Monday")).toBe(false);
+        expect(areWeCovered([], "Tuesday")).toBe(false);
+        expect(areWeCovered([], "Wednesday")).toBe(false);
+        expect(areWeCovered([], "Thursday")).toBe(false);
+        expect(areWeCovered([], "Friday")).toBe(false);
+        expect(areWeCovered([], "Saturday")).toBe(false);
+        expect(areWeCovered([], "Sunday")).toBe(false);
+        
+    });
+})
